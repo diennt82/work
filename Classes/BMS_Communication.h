4ad3797916a7b8061838f034d60adb4c0e49950d
Phung Nguyen <triphung@nxcomm.com>
2012-11-30 16:59:15 +0800

    - upgrade version

--- Classes/BMS_Communication.h
+++ Classes/BMS_Communication.h
@@ -10,6 +10,7 @@
 #import "Util.h"
 
 #define BMS_DEFAULT_TIME_OUT 30
+//#define BMS_PHONESERVICE @"https://simplimonitor.com/BMS/phoneservice?"
 #define BMS_PHONESERVICE @"https://monitoreverywhere.com/BMS/phoneservice?"
 #define BMS_CMD_PART     @"action=command&command="
 
