8a21fd562a7a7cda1e2dffa29e6310cfc7535af4
Ankita <ankitag612@gmail.com>
2013-03-11 16:46:08 +0700

    Filter empty ssid

--- Classes/Step_05_ViewController.mm
+++ Classes/Step_05_ViewController.mm
@@ -87,7 +87,7 @@
     {
         WifiEntry * wifi = [listOfWifi objectAtIndex:i];
 //        NSLog(@"SSID Wifi -------------------->%@", wifi.ssid_w_quote);
-        if (![wifi.ssid_w_quote hasPrefix:@"\"Camera-"])
+        if (![wifi.ssid_w_quote hasPrefix:@"\"Camera-"] && ![wifi.ssid_w_quote isEqualToString:@"\"\""])
         {
             [wifiList addObject:wifi];
             
