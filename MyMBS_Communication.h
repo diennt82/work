4c95da021b4853df884b20e8e3567aae9438ea1d
Luan <luan.nguyen@nxcomm.com>
2013-08-08 08:49:37 +0700

    Create new communication methods for new API(JSON): users + some Devices methods

---
+++ MyMBS_Communication.h
@@ -0,0 +1,13 @@
+//
+//  MyMBS_Communication.h
+//  MBP_ios
+//
+//  Created by NxComm on 6/8/13.
+//  Copyright (c) 2013 Smart Panda Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyMBS_Communication : NSObject
+
+@end
---
+++ MyMBS_Communication.m
@@ -0,0 +1,13 @@
+//
+//  MyMBS_Communication.m
+//  MBP_ios
+//
+//  Created by NxComm on 6/8/13.
+//  Copyright (c) 2013 Smart Panda Ltd. All rights reserved.
+//
+
+#import "MyMBS_Communication.h"
+
+@implementation MyMBS_Communication
+
+@end
