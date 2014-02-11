e639dec53fe8f0e2e0f0660f0e7e65061275c5c5
Luan <luan.nguyen@nxcomm.com>
2014-02-11 17:31:53 +0700

    Add new CameraMenuVC to replace MenuCameraVC

---
+++ Classes/CameraMenuViewController.h
@@ -0,0 +1,18 @@
+//
+//  CameraMenuViewController.h
+//  BlinkHD_ios
+//
+//  Created by Developer on 2/11/14.
+//  Copyright (c) 2014 Smart Panda Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CameraScanner/CameraScanner.h>
+#import "ConnectionMethodDelegate.h"
+
+@interface CameraMenuViewController : UIViewController
+
+@property (nonatomic, assign) id<ConnectionMethodDelegate> menuCamerasDelegate;
+@property (nonatomic, retain) CamChannel *camChannel;
+
+@end
---
+++ Classes/CameraMenuViewController.m
@@ -0,0 +1,336 @@
+//
+//  CameraMenuViewController.m
+//  BlinkHD_ios
+//
+//  Created by Developer on 2/11/14.
+//  Copyright (c) 2014 Smart Panda Ltd. All rights reserved.
+//
+
+#define ALERT_REMOVE_CAM 5
+
+#define ALERT_REMOVE_CAM_LOCAL 6
+#define ALERT_REMOVE_CAM_REMOTE 7
+
+#import "CameraMenuViewController.h"
+#import "CameraSettingsCell.h"
+#import "CameraNameViewController.h"
+#import <MonitorCommunication/MonitorCommunication.h>
+
+@interface CameraMenuViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (retain, nonatomic) IBOutlet UITableView *tableViewSettings;
+@property (retain, nonatomic) IBOutlet UIButton *btnRmoveCamera;
+@property (retain, nonatomic) IBOutlet UIView *viewProgress;
+
+@property (retain, nonatomic) NSString *stringFW_Version;
+
+@end
+
+@implementation CameraMenuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        self.title = @"Camera Settings";
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    self.tableViewSettings.delegate = self;
+    self.tableViewSettings.dataSource = self;
+    
+    [self.btnRmoveCamera setBackgroundImage:[UIImage imageNamed:@"remove_camera"] forState:UIControlStateNormal];
+    [self.btnRmoveCamera setBackgroundImage:[UIImage imageNamed:@"remove_camera_pressed"] forState:UIControlEventTouchDown];
+    
+    self.stringFW_Version = NSLocalizedStringWithDefaultValue(@"firmware_version",nil, [NSBundle mainBundle],
+                                                   @"Firmware version", nil);
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBarHidden = NO;
+    
+    [self.tableViewSettings reloadRowsAtIndexPaths:[NSArray arrayWithObject:[NSIndexPath indexPathForRow:0 inSection:0]] withRowAnimation:UITableViewRowAnimationAutomatic];
+}
+
+- (IBAction)btnRemoveCameraTouchUpInsideAction:(id)sender
+{
+    self.navigationItem.hidesBackButton = YES;
+    self.view.userInteractionEnabled = NO;
+    self.btnRmoveCamera.enabled = NO;
+    
+    self.viewProgress.frame = UIScreen.mainScreen.bounds;
+    self.viewProgress.hidden = NO;
+    [self.view bringSubviewToFront:_viewProgress];
+    
+    [self  showDialog:ALERT_REMOVE_CAM];
+}
+
+- (void) showDialog:(int) dialogType
+{
+    NSString * ok = NSLocalizedStringWithDefaultValue(@"Ok",nil, [NSBundle mainBundle],
+                                                      @"Ok", nil);
+    NSString * cancel = NSLocalizedStringWithDefaultValue(@"Cancel",nil, [NSBundle mainBundle],
+                                                          @"Cancel", nil);
+    
+	switch (dialogType) {
+            
+		case ALERT_REMOVE_CAM:
+		{
+			BOOL deviceInLocal = _camChannel.profile.isInLocal;
+            
+            if (deviceInLocal)
+            {
+                NSString * msg = NSLocalizedStringWithDefaultValue(@"Confirm_remove_cam_local",nil, [NSBundle mainBundle],
+                                                                   @"Please confirm that you want to remove this camera from your account. This action will also reset the camera to setup mode.", nil);
+                UIAlertView *alert = [[UIAlertView alloc]
+                                      initWithTitle:@""
+                                      message:msg
+                                      delegate:self
+                                      cancelButtonTitle:cancel
+                                      otherButtonTitles:ok,nil];
+                alert.tag = ALERT_REMOVE_CAM_LOCAL;
+                [alert show];
+                [alert release];
+                
+            }
+            else
+            {
+                NSString * msg = NSLocalizedStringWithDefaultValue(@"Confirm_remove_cam_remote",nil, [NSBundle mainBundle],
+                                                                   @"Please confirm that you want to remove this camera from your account. The camera is not accessible right now, it will not be switched to setup mode. Please refer to FAQ to reset it manually.", nil);
+                
+                
+                UIAlertView *alert = [[UIAlertView alloc]
+                                      initWithTitle:@""
+                                      message:msg
+                                      delegate:self
+                                      cancelButtonTitle:cancel
+                                      otherButtonTitles:ok,nil];
+                
+                alert.tag = ALERT_REMOVE_CAM_REMOTE;
+                [alert show];
+                [alert release];
+            }
+            
+			break;
+		}
+            
+		default:
+			break;
+	}
+}
+
+#pragma  mark - Alert Delegate
+
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == 1)
+    {
+        
+        int tag = alertView.tag ;
+        
+        if (tag == ALERT_REMOVE_CAM_LOCAL)
+        {
+            [self removeLocalCamera];
+        }
+        else if (tag == ALERT_REMOVE_CAM_REMOTE)
+        {
+            [self removeRemoteCamera];
+        }
+    }
+    else
+    {
+        self.viewProgress.hidden = YES;
+        
+        self.navigationItem.hidesBackButton = NO;
+        self.view.userInteractionEnabled = YES;
+        self.btnRmoveCamera.enabled = YES;
+    }
+}
+
+-(void) removeLocalCamera
+{
+    HttpCommunication * dev_comm = [[HttpCommunication alloc]init];
+    dev_comm.device_ip = _camChannel.profile.ip_address;
+    dev_comm.device_port = _camChannel.profile.port;
+    
+	NSString * command = SWITCH_TO_DIRECT_MODE;
+	[dev_comm sendCommandAndBlock:command];
+	
+	command = RESTART_HTTP_CMD;
+	[dev_comm sendCommandAndBlock:command];
+    
+    [dev_comm release];
+    
+    [self removeRemoteCamera];
+}
+
+-(void) removeRemoteCamera
+{
+    BMS_JSON_Communication *jsonComm = [[[BMS_JSON_Communication alloc] initWithObject:self
+                                                                              Selector:@selector(removeCameraSuccessWithResponse:)
+                                                                          FailSelector:@selector(removeCameraFailedWithError:)
+                                                                             ServerErr:@selector(removeCameraFailedServerUnreachable)] autorelease];
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    //NSString *mac = [Util strip_colon_fr_mac:_camChannel.profile.mac_address];
+    
+    [jsonComm deleteDeviceWithRegistrationId:_camChannel.profile.registrationID
+                                   andApiKey:[userDefaults objectForKey:@"PortalApiKey"]];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    //#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    //#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == 0)
+    {
+        static NSString *CellIdentifier = @"CameraSettingsCell";
+        CameraSettingsCell *cell = [self.tableViewSettings dequeueReusableCellWithIdentifier:CellIdentifier];
+        
+        NSArray *objects = [[NSBundle mainBundle] loadNibNamed:@"CameraSettingsCell" owner:nil options:nil];
+        
+        for (id curObj in objects)
+        {
+            
+            if([curObj isKindOfClass:[UITableViewCell class]])
+            {
+                cell = (CameraSettingsCell *)curObj;
+                break;
+            }
+        }
+        
+        
+        cell.nameLabel.text = @"Name";
+        cell.valueLabel.text = self.camChannel.profile.name;
+        
+        return cell;
+    }
+    else if (indexPath.row == 1)
+    {
+        static NSString *CellIdentifier = @"Cell";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        if (cell == nil) {
+            cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+        }
+        
+        // Configure the cell...
+        cell.textLabel.text = @"Change Image";
+        
+        return cell;
+    }
+    else
+    {
+        // display firm ware version
+        static NSString *CellIdentifier = @"CameraSettingsCell";
+        CameraSettingsCell *cell = [self.tableViewSettings dequeueReusableCellWithIdentifier:CellIdentifier];
+        
+        NSArray *objects = [[NSBundle mainBundle] loadNibNamed:@"CameraSettingsCell" owner:nil options:nil];
+        
+        for (id curObj in objects)
+        {
+            
+            if([curObj isKindOfClass:[UITableViewCell class]])
+            {
+                cell = (CameraSettingsCell *)curObj;
+                break;
+            }
+        }
+        
+        
+        cell.nameLabel.text = _stringFW_Version;
+        cell.valueLabel.text = self.camChannel.profile.fw_version;
+        
+        return cell;
+    }
+}
+
+
+#pragma mark - Table view delegate
+
+// In a xib-based application, navigation from a table can be handled in -tableView:didSelectRowAtIndexPath:
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here, for example:
+    // Create the next view controller.
+    
+    if (indexPath.row == 0)
+    {
+        CameraNameViewController *cameraNameViewController = [[CameraNameViewController alloc] initWithNibName:@"CameraNameViewController"
+                                                                                                        bundle:nil];
+        
+        // Pass the selected object to the new view controller.
+        
+        // Push the view controller.
+        cameraNameViewController.cameraName = self.camChannel.profile.name;
+        [self.navigationController pushViewController:cameraNameViewController animated:YES];
+        
+        [cameraNameViewController release];
+    }
+}
+
+#pragma BMS_JSON delegate
+
+- (void) removeCameraSuccessWithResponse:(NSDictionary *)responseData
+{
+	NSLog(@"CameraMenuVC- removeCam success-- fatality");
+    
+    //[self forceRelogin];
+	//[self.navigationController popViewControllerAnimated:YES];
+    [self.navigationController popToRootViewControllerAnimated:YES];
+    
+    [_menuCamerasDelegate sendStatus:AFTER_DEL_RELOGIN];
+}
+
+- (void) removeCameraFailedWithError:(NSDictionary *)errorResponse
+{
+	NSLog(@"CameraMenuVC - removeCam failed errorcode:");
+    [self.navigationController popToRootViewControllerAnimated:YES];
+    
+    [_menuCamerasDelegate sendStatus:AFTER_DEL_RELOGIN];
+}
+
+-(void) removeCameraFailedServerUnreachable
+{
+	NSLog(@"CameraMenuVC - removeCam server unreachable");
+    //[self forceRelogin];
+    //[self.navigationController popViewControllerAnimated:YES];
+    
+    [self.navigationController popToRootViewControllerAnimated:YES];
+    
+    [_menuCamerasDelegate sendStatus:AFTER_DEL_RELOGIN];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc {
+    [_tableViewSettings release];
+    [_btnRmoveCamera release];
+    [_viewProgress release];
+    [super dealloc];
+}
+@end
