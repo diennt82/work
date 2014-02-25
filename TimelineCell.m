cf7c7f47971489ff10980a6b2e70ed185b2e1145
Luan <luan.nguyen@nxcomm.com>
2014-02-25 17:55:20 +0700

    Timeline view: white dot is overlapped. Handle low-temperature alert type

--- TimelineCell.m
+++ TimelineCell.m
@@ -29,11 +29,12 @@
 - (void)drawRect:(CGRect)rect
 {
     UIImageView *imageViewWhiteDot = (UIImageView *)[self viewWithTag:950];
-//    imageViewWhiteDot.frame = CGRectMake(imageViewWhiteDot.frame.origin.x, _eventDetailLabel.center.y + _eventDetailLabel.frame.size.height / 2, imageViewWhiteDot.frame.size.width, imageViewWhiteDot.frame.size.height);
-    imageViewWhiteDot.center = CGPointMake(imageViewWhiteDot.center.x, imageViewWhiteDot.center.y + _eventDetailLabel.frame.size.height / 2);
+    imageViewWhiteDot.frame = CGRectMake(imageViewWhiteDot.frame.origin.x, _eventDetailLabel.center.y + _eventDetailLabel.frame.size.height / 2, imageViewWhiteDot.frame.size.width, imageViewWhiteDot.frame.size.height);
+    //imageViewWhiteDot.center = CGPointMake(imageViewWhiteDot.center.x, imageViewWhiteDot.center.y + _eventDetailLabel.frame.size.height / 2);
     
     UIImageView *imageViewLine = (UIImageView *)[self viewWithTag:951];
-    imageViewLine.center = CGPointMake(imageViewLine.center.x, imageViewLine.center.y + _eventDetailLabel.frame.size.height / 2);
+    imageViewLine.frame = CGRectMake(imageViewLine.frame.origin.x, imageViewWhiteDot.center.y + imageViewWhiteDot.frame.size.width / 2, imageViewLine.frame.size.width, imageViewLine.frame.size.height);
+   // imageViewLine.center = CGPointMake(imageViewLine.center.x, imageViewLine.center.y + _eventDetailLabel.frame.size.height / 2);
 }
 
 - (void)dealloc {
