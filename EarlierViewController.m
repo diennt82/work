3fc158253722e7c8b1a472e779e6a060c61d5a0b
Luan <luan.nguyen@nxcomm.com>
2014-04-08 13:15:47 +0700

    Fix error displaying timeline view

--- EarlierViewController.m
+++ EarlierViewController.m
@@ -9,6 +9,7 @@
 #import "EarlierViewController.h"
 #import "SavedEventViewController.h"
 #import "define.h"
+#import "PublicDefine.h"
 
 @interface EarlierViewController ()
 
@@ -31,7 +32,10 @@
 {
     self = [super init];
     
-    self.camChannel = camChannel;
+    if (self)
+    {
+        self.camChannel = camChannel;
+    }
     
     return self;
 }
@@ -40,7 +44,21 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    self.navigationController.navigationBarHidden = YES;
+    
+    if ([_camChannel.profile isNotAvailable])
+    {
+        self.navigationItem.hidesBackButton = YES;
+        self.navigationController.navigationBarHidden = NO;
+        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Earlier"
+                                                                                  style:UIBarButtonItemStylePlain
+                                                                                 target:nil
+                                                                                 action:nil];
+        [self addHubbleLogo_Back];
+    }
+    else
+    {
+        self.navigationController.navigationBarHidden = YES;
+    }
     
     self.timelineVC = [[TimelineViewController alloc] init];
     SavedEventViewController *savedViewController = [[SavedEventViewController alloc] initWithNibName:@"SavedEventViewController" bundle:nil];
@@ -62,19 +80,57 @@
         if ([self.camChannel.profile isNotAvailable])
         {
             self.view.frame = CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+            self.timelineVC.navVC = self.navigationController;
         }
+        else
+        {
+            self.timelineVC.navVC = _nav;
+        }
+        
         self.isDidLoad = TRUE;
         [_timelineVC loadEvents:_camChannel];
-        self.timelineVC.navVC = _nav;
-        if ((isiPhone4 || isiPhone5))
-        {
-            self.timelineVC.tableView.contentInset = UIEdgeInsetsMake(5, 0, 44, 0);
-        } else
-        {
-            self.timelineVC.tableView.contentInset = UIEdgeInsetsMake(5, 0, 64, 0);
-        }
+        
+        self.timelineVC.tableView.contentInset = UIEdgeInsetsMake(5, 0, 64, 0);
     }
 }
+
+#pragma mark - Methods
+
+-(void)addHubbleLogo_Back
+{
+    UIImage *image = [UIImage imageNamed:@"Hubble_back_text"];
+    CGRect frame = CGRectMake(0, 0, image.size.width, image.size.height);
+    
+    //init a normal UIButton using that image
+    UIButton* button = [[UIButton alloc] initWithFrame:frame];
+    [button setBackgroundImage:image forState:UIControlStateNormal];
+    [button setShowsTouchWhenHighlighted:YES];
+    
+    //set the button to handle clicks - this one calls a method called 'downloadClicked'
+    [button addTarget:self action:@selector(goBackToHubbleMenu:) forControlEvents:UIControlEventTouchUpInside];
+    
+    //finally, create your UIBarButtonItem using that button
+    UIBarButtonItem* barButtonItem = [[UIBarButtonItem alloc] initWithCustomView:button];
+    
+    //then set it.  phew.
+    [self.navigationItem setLeftBarButtonItem:barButtonItem];
+    
+    [barButtonItem release];
+}
+
+- (void)goBackToHubbleMenu: (id)sender
+{
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    [userDefaults removeObjectForKey:CAM_IN_VEW];
+    [userDefaults synchronize];
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (BOOL)shouldAutorotate
+{
+    return NO;
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -103,6 +159,7 @@
 - (void)mh_tabBarController:(MHTabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController atIndex:(NSUInteger)index
 {
 	NSLog(@"mh_tabBarController %@ didSelectViewController %@ at index %u", tabBarController, viewController, index);
+     //viewController.view.frame = CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 }
 
 
