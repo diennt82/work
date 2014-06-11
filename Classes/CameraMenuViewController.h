e8e8cba48d4f901aa1d53dd9a26d7fc4dcf8e3bf
Sven Resch <sven@logicopolis.com>
2014-06-11 16:13:55 -0700

    Changed view to tighten up the main table view. Code reformatting.

--- Classes/CameraMenuViewController.h
+++ Classes/CameraMenuViewController.h
@@ -3,7 +3,7 @@
 //  BlinkHD_ios
 //
 //  Created by Developer on 2/11/14.
-//  Copyright (c) 2014 Smart Panda Ltd. All rights reserved.
+//  Copyright (c) 2014 eBuyNow eCommerce Limited. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
@@ -20,6 +20,6 @@
 @property (nonatomic, assign) NSString *cameraName;
 @property (nonatomic, assign) id<ConnectionMethodDelegate> cameraMenuDelegate;
 @property (nonatomic, assign) CamChannel *camChannel;
-@property (retain, nonatomic) IBOutlet UIView *viewPorgress;
+@property (nonatomic, retain) IBOutlet UIView *viewPorgress;
 
 @end
