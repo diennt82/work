96c3e3337f263be9913e565be05607bbe445783d
Sven Resch <sven@logicopolis.com>
2014-08-07 10:37:43 -0700

    Fixed project setup issue that caused compile error for normal Hubble app target. Minor formatting of main.m.

--- main.m
+++ main.m
@@ -3,13 +3,13 @@
 //  MBP_ios
 //
 //  Created by NxComm on 9/26/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Hubble Connected Ltd. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-int main(int argc, char *argv[]) {
-    
+int main(int argc, char *argv[])
+{
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     int retVal = UIApplicationMain(argc, argv, nil, nil);
     [pool release];
