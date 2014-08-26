ffceec1166ee2108cf4b6fc843f9c1fd6c30f92a
Phuoc Duy <phuocduy.it@gmail.com>
2014-08-26 17:25:26 +0700

    Create new file Congrat popup

---
+++ Classes/Helps/CongratHelpWindowPopup.h
@@ -0,0 +1,13 @@
+//
+//  CongratHelpWindowPopup.h
+//  BlinkHD_ios
+//
+//  Created by Developer on 26/8/14.
+//  Copyright (c) 2014 Smart Panda Ltd. All rights reserved.
+//
+
+#import "HelpWindowPopup.h"
+
+@interface CongratHelpWindowPopup : HelpWindowPopup
+
+@end
